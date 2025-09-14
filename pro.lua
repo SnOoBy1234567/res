33c5b9408c304278aef02c2343c9afe921a49b54
SnOoBy <sadkomodo9@gmail.com>
2025-09-15 02:21:40 +0300

    Update pro.lua

--- pro.lua
+++ pro.lua
@@ -193,6 +193,72 @@ local function fireAtPart(targetPart)
     FireEvent:FireServer(unpack(args))
 end
 
+local function onTargetSit()
+    -- Credits by Pio "Discord User id: 311397526399877122"
+    -- and edited by Agent666_0 (with ai helper)
+    local r_time = game.Players.RespawnTime
+
+    local lp = game.Players.LocalPlayer
+    local bp = lp.Backpack
+    local chr = lp.Character
+
+    local rhand = chr:WaitForChild("RightHand") -- r6 için: WaitForChild("Right Arm")
+
+    local function setsimradius(radius)
+        lp.MaximumSimulationRadius = radius
+        lp.SimulationRadius = radius
+    end
+
+    local tool = bp:FindFirstChildOfClass("Tool")
+    if not tool then return end
+    local t_handle = tool.Handle
+
+    tool.Parent = chr
+    tool.Parent = bp
+
+    chr.Humanoid.Sit = false
+    chr.Humanoid.RootPart.CFrame = CFrame.new(0, -499, 0) * CFrame.Angles(0, 0, math.rad(90))
+
+    rhand:GetPropertyChangedSignal("Parent"):Connect(function()
+        if not rhand.Parent then
+            workspace.Camera.CameraSubject = t_handle
+            setsimradius(9e6)
+
+            local bp_inst = Instance.new("BodyPosition")
+            bp_inst.Position = tool.Handle.Position + Vector3.new(0, 20, 0)
+            bp_inst.MaxForce = Vector3.one * 9e10
+            bp_inst.P = 9e4
+            bp_inst.Parent = tool.Handle
+
+            t_handle.CanCollide = false
+            t_handle.CanQuery = false
+            tool.Parent = chr
+
+            repeat task.wait() until (t_handle.Position - bp_inst.Position).Magnitude < 5
+
+            for i, v in next, game.Players:GetPlayers() do
+                local v_chr = v.Character
+                if i > 1 and v_chr then
+                    local v_hum = v_chr:FindFirstChildOfClass("Humanoid")
+                    if v_hum and not v_hum.Sit then
+                        local v_root = v_hum.RootPart
+                        if v_root and v_root.Velocity.Magnitude < 600 then
+                            for i = 1, r_time + 3 do
+                                task.wait()
+                                tool.Handle.RotVelocity = Vector3.new(8000, 8000, -8000)
+                                t_handle.Position = v_root.Position + (v_hum.MoveDirection * 3.8)
+                                bp_inst.Position = t_handle.Position
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end)
+end
+
+
+			
 -- Atira em todos os jogadores
 local function fireAtAllPlayers(times)
     for i = 1, times do
@@ -958,7 +1024,12 @@ local function BringPlayerLLL()
         local startTime = tick()
         while tick() - startTime < 5 and target and target.Character and target.Character:FindFirstChildOfClass("Humanoid") do
             local tHum = target.Character:FindFirstChildOfClass("Humanoid")
-            if not tHum or tHum.Sit then break end
+            if tHum.Sit then
+    print("niga")  -- opsiyonel
+    onTargetSit() -- buraya ikinci kodu çağırıyoruz
+    break
+end
+
 
             local hrp = target.Character.HumanoidRootPart
             local adjustedPos = hrp.Position + (hrp.Velocity / 1.5)
